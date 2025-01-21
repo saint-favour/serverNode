d889703e49339d5cc6ee838e2b1662ef80473815
saint-favour <magnifisaintama@gmail.com>
2025-01-21 12:57:21 +0100

    deploy_routes from local server

--- server.js
+++ server.js
@@ -1,10 +1,51 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    console.log('request made')
+     console.log(req.url, req.method)
+
+    res.setHeader('Content-Type', 'text/html')
+
+    
+let path = './views/'
+switch(req.url) {
+    case '/' :
+        path += 'index.html'
+        break;
+    case '/about':
+        path += 'about.html'
+        break;
+    default:
+        path += '404.html'
+        break; 
+
+}
+
+// this code get path partially works, the else doesn't get the 
+// 404 page, but insted gives the root file 
+    /*let path = './views/'
+if(req.url = '/'){
+    path += 'index.html'
+}
+else if(req.url = '/about'){
+    path += 'about.html'
+}else{
+    path += '404.html'
+}*/
+
+    fs.readFile(path, (err, data) =>{
+       if(err){
+        console.log(err)
+        res.end()
+       } else{
+        res.write(data)
+        res.end()
+       }
+    })
 })
 
 server.listen(3000, 'localhost', () =>{
-    console.log('listening for request on port 3000')
+    console.log('listening for request on localhost:3000')
 })
- 
+  
+
