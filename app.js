d8226f6e5a82b679f769a9e64ebdaf7bb5043d18
saint-favour <magnifisaintama@gmail.com>
2025-01-22 20:06:44 +0100

    intro to the middleware using express. know where and when to use it

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ app.listen(3000)
 console.log('listenig on localhost:3000')
 
 
+app.use((req, res, next) => {
+    console.log('new request made:')
+    console.log('host:', req.hostname)
+    console.log('path:', req.path)
+    console.log('method:', req.method)
+    next()
+}) 
+
+
 
 app.get('/', (req, res) =>{
     const thoughts = [
